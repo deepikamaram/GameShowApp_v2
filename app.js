a1deea5020b33e63134a91f98dcf07de71336b08
Deepika <thewebdevworld@gmail.com>
2021-09-09 19:54:27 -0500

    event bubbling

--- app.js
+++ app.js
@@ -1,26 +1,42 @@
 const qwerty = document.getElementById("qwerty");
 const phraseElement = document.getElementById("phrase");
-const keyboardButtons = document.querySelector(".keyrow").children;
+const overlay = document.querySelector("#overlay");
+const btnReset = document.querySelector(".btn__reset");
 let missed = 0;
 
-document.querySelector(".btn__reset").addEventListener("click", () => {
-  document.querySelector("#overlay").style.display = "none";
+btnReset.addEventListener("click", () => {
+  if (btnReset.textContent === "Start Game") {
+    overlay.style.display = "none";
+  }
+  if (
+    btnReset.textContent === "Play Again" ||
+    btnReset.textContent === "Retry"
+  ) {
+    overlay.classList.remove("win");
+    btnReset.classList.remove("win-a");
+    overlay.classList.remove("lose");
+    btnReset.classList.remove("lose-a");
+    document.querySelector("h2").textContent = "Wheel of Success";
+    btnReset.textContent = "Start Game";
+    missed = 0;
+  }
 });
 
 const phrases = [
   "test",
-  // "test",
-  // "a drop in the ocean",
-  // "never give up",
-  // "and still i rise",
-  // "believe in yourself",
-  // "never look back",
+  "a drop in the ocean",
+  "never give up",
+  "and still i rise",
+  "believe in yourself",
+  "never look back",
 ];
 
 function getRandomPhraseAsArray(arr) {
   const ranNum = Math.floor(Math.random() * arr.length);
   return arr[ranNum].split("");
 }
+
+const randomPhrase = getRandomPhraseAsArray(phrases);
 
 function addPhraseToDisplay(arr) {
   for (let i = 0; i < arr.length; i++) {
@@ -35,8 +51,6 @@ function addPhraseToDisplay(arr) {
   }
 }
 
-const randomPhrase = getRandomPhraseAsArray(phrases);
-console.log(randomPhrase);
 addPhraseToDisplay(randomPhrase);
 
 function checkLetter(button) {
@@ -57,38 +71,48 @@ function checkWin() {
   const showLen = document.querySelectorAll(".show").length;
   const phraseHolLen = document.querySelectorAll(".phrase-holder").length;
 
-  if (showLen === phraseHolLen && missed <= 5) {
-    document.querySelector("#overlay").style.display = "flex";
-    document.querySelector("#overlay").classList.add("win");
-    document.querySelector(".btn__reset").classList.add("win-a");
-    document.querySelector(".btn__reset").textContent = "Re-match";
-    const p = document.createElement("p");
-    p.textContent = "Yuppie you win!";
-    document.querySelector("#overlay").appendChild(p);
+  if (showLen === phraseHolLen && missed < 5) {
+    overlay.style.display = "flex";
+    overlay.classList.add("win");
+    btnReset.classList.add("win-a");
+    btnReset.textContent = "Play Again";
+    document.querySelector("h2").textContent = "You Won!";
+    // const p = document.createElement("p");
+    // p.textContent = "Yuppie you win!";
+    // overlay.appendChild(p);
   } else if (missed === 5) {
-    document.querySelector("#overlay").style.display = "flex";
-    document.querySelector("#overlay").classList.add("lose");
-    document.querySelector(".btn__reset").classList.add("lose-a");
-    document.querySelector(".btn__reset").textContent = "Retry";
-    const p = document.createElement("p");
-    p.textContent = "Oops You lost!";
-    document.querySelector("#overlay").appendChild(p);
+    overlay.style.display = "flex";
+    overlay.classList.add("lose");
+    btnReset.classList.add("lose-a");
+    btnReset.textContent = "Retry";
+    document.querySelector("h2").textContent = "You Lost!";
+    // const p = document.createElement("p");
+    // p.textContent = "Oops You lost!";
+    // overlay.appendChild(p);
   }
 }
 
-qwerty.addEventListener("keydown", (e) => {
-  const userKey = e.target.textContent;
-  e.target.disabled = true;
+window.addEventListener("keypress", (e) => {
+  const userKey = e.key;
   const letter = checkLetter(userKey);
-  if (letter) {
-    e.target.className = "correct";
-  } else {
-    e.target.className = "choosen";
+
+  if (!letter) {
     missed++;
-    console.log(missed);
     const hearts = document.querySelectorAll(".tries img");
     hearts[missed - 1].src = "images/lostHeart.png";
-    console.log(hearts[missed - 1]);
+  }
+
+  const btns = document.getElementsByTagName("button");
+  for (let i = 0; i < btns.length; i++) {
+    const key = btns[i];
+
+    if (letter === key.textContent) {
+      key.className = "correct";
+      key.disabled = true;
+    } else if (userKey === key.textContent) {
+      key.className = "wrong";
+      key.disabled = true;
+    }
   }
 
   checkWin();
